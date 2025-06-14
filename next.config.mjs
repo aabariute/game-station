ff987260417e5216e990b822945bb2d128212229
Ausrine Abariute <a.abariute@gmail.com>
2025-06-14 17:56:17 +0300

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "qzjoaseklvxaztlbgrmt.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/product-images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
